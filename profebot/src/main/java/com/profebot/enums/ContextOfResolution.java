121c143511f0e2eba6ddc03d9a1ec43032381a73
Ignacio Casciaro <icasciaro@est.frba.utn.edu.ar>
2022-09-26 22:09:17 -0300

    chore: clean code

--- profebot/src/main/java/com/profebot/enums/ContextOfResolution.java
+++ profebot/src/main/java/com/profebot/enums/ContextOfResolution.java
@@ -7,9 +7,4 @@ public class ContextOfResolution {
 
     public static final String CONTEXT_OF_RESOLUTION_IS_INEQUATION_WITH_INTERVAL_SOLUTIONS = "inequation_interval_solutions";
     public static final String CONTEXT_OF_RESOLUTION_IS_INEQUATION_WITHOUT_SOLUTIONS = "inequation_without_solutions";
-
-    public static final String CONTEXT_OF_RESOLUTION_IS_DOMAIN = "domain";
-    public static final String CONTEXT_OF_RESOLUTION_IS_IMAGE = "image";
-    public static final String CONTEXT_OF_RESOLUTION_IS_ROOTS = "roots_of_function";
-    public static final String CONTEXT_OF_RESOLUTION_IS_ORIGIN_ORD = "origin_ord_of_function";
 }
